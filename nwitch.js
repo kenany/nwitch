7cdc277309074b10266abd6351c4787416360e1a
Kenan Yildirim <kenan@kenany.me>
2013-09-05 19:31:22 -0400

    Re-activate dashboard

--- nwitch.js
+++ nwitch.js
@@ -1,4 +1,8 @@
 var Bot = require('./lib/core/bot');
+var App = require('./lib/core/app');
 
 var nwitch = new Bot('./config.toml');
+var dashboard = new App(nwitch.getConfig());
+
 nwitch.spawn();
+dashboard.spawn();
