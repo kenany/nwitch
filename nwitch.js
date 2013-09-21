8d35f6152061836f9303b6f3378392a7eec3428c
Kenan Yildirim <kenan@kenany.me>
2013-09-21 14:33:00 -0400

    Go back to using `lib/index.js`

--- nwitch.js
+++ nwitch.js
@@ -1,8 +1,1 @@
-var Bot = require('./lib/core/bot');
-var App = require('./lib/core/app');
-
-var nwitch = new Bot('./config.toml');
-var dashboard = new App(nwitch.getConfig());
-
-nwitch.spawn();
-dashboard.spawn();
+require('./lib/')();
