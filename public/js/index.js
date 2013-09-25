1181b6ca1fe0a36b24305d615123b884e9c81b5d
Kenan Yildirim <kenan@kenany.me>
2013-09-25 18:10:31 -0400

    Create sidebar

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
-// scripts
+var Snap = require('Snap.js');
+
+var snapper = new Snap({
+  element: document.getElementById('content'),
+  disable: 'right'
+});
+
+snapper.open('left');
+snapper.disable();
