dc630c0e0b780c33b762b1eddc0bac1d8d8f38a6
Kenan Yildirim <kenan@kenany.me>
2013-11-03 15:25:20 -0500

    Add nwitch bin utility

---
+++ bin/cmd.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var path = require('path');
+var findup = require('findup-sync');
+var resolve = require('resolve').sync;
+
+var base = process.cwd();
+
+var nwitchPath;
+try {
+  nwitchPath = resolve('nwitchPath', {basedir: base});
+} catch (error) {
+  nwitchPath = findup('lib/index.js');
+  if (!nwitchPath) {
+    console.error('Unable to find local nwitch.');
+  }
+}
+
+require(nwitchPath)(path.resolve(base, './config.toml'));
