ef56508154b55ba9216d8e903b6a199689de3a01
Kenan Yildirim <kenan@kenany.me>
2014-11-28 09:01:04 -0500

    Do setup and cleanup
    Closes #39

--- test/index.js
+++ test/index.js
@@ -2,17 +2,22 @@ var Nwitch = require('../');
 var test = require('tape');
 var ircd = require('ircd');
 
+var irc;
+
+test('setup', function(t) {
+  irc = ircd.createServer({name: 'localhost'});
+  irc.listen(51402);
+  t.end();
+});
+
 test('joins channel', function(t) {
   t.plan(2);
 
-  var irc = ircd.createServer({name: 'localhost'});
-  irc.listen(51402);
   irc.on('channel', function(channel) {
     channel.on('join', function(user) {
       t.equal(user.nick, 'kenan');
       t.equal(channel.name, '#nwitch');
       n.quit();
-      irc.close();
     });
   });
 
@@ -27,4 +32,9 @@ test('joins channel', function(t) {
       channel: 'nwitch'
     }
   });
+});
+
+test('cleanup', function(t) {
+  irc.close();
+  t.end();
 });
